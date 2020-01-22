f1618ad6a92d2abad0a1f92b3b42622e2b71064b
Clay Sullivan <clay.r.sullivan-1@ou.edu>
2020-01-22 11:09:56 -0600

    Created initialsolution for in class example

---
+++ InClassExample/InClassExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// Clay Sullivan
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

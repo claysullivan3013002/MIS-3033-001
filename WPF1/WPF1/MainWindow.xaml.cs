86fa531df3352772f842a16c1cef3d87dd0234c5
Clay Sullivan <clay.r.sullivan-1@ou.edu>
2020-01-29 11:46:35 -0600

    kind of done

--- WPF1/WPF1/MainWindow.xaml.cs
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Clay Sullivan
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,49 @@ namespace WPF1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+                             
+            
+        }
+        private void TextBox_TextChanged2(object sender, TextChangedEventArgs e)
+        {
+            
+
+            
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm.name = textbox1.Text;
+            string address = textbox2.Text;
+            int zip = Convert.ToInt32(textbox3.Text);
+            listbox1.Items.Add(EntryForm);
+            listbox1.Items.Add(zip);
+        }
+
+        private void listbox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            
+            
+        }
+
+        private void textbox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
+
 }
